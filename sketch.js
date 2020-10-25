641ce243c57337f278af72476a0856a3c9758114
shivonPunia <67789366+shivonPunia@users.noreply.github.com>
2020-10-25 11:59:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+ var raindrops = [];
+ var umbrella;
+
+function setup() {
+  engine = Engine.create();
+  world = engine.world;
+  var canvas = createCanvas(400,800);
+  umbrella = new Umbrella(400, 790, 285);
+
+}
+
+function draw() {
+  background("black");
+ 
+ // engine.update();
+
+  if(frameCount % 1 === 0){
+    var raindrop = new Raindrops(random(0, windowWidth), -100,10);
+    raindrops.push(raindrop);
+  }
+
+  for(var i = 0; i < raindrops.length; i++){
+    raindrops[i].fall(40);
+    raindrops[i].velocityY++
+    raindrops[i].display();
+  }
+  umbrella.display(); 
+}
